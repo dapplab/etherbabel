e47d64e243c5cb5fe398d5a6f4621a1e63bb44b1
u2 <zhangyaning1985@gmail.com>
2015-12-05 19:01:42 +0800

    clear unused redux

--- dapp/app/src/app.js
+++ dapp/app/src/app.js
@@ -1,23 +1,11 @@
 import React                  from 'react';
 import ReactDOM               from 'react-dom';
-import createBrowserHistory   from 'history/lib/createBrowserHistory';
-import { syncReduxAndRouter } from 'redux-simple-router';
 import Root                   from './containers/Root';
-import configureStore         from './store/configureStore';
 
 const target  = document.getElementById('root');
-const history = createBrowserHistory();
-const store   = configureStore(window.__INITIAL_STATE__, __DEBUG__);
-
-syncReduxAndRouter(history, store);
 
 const node = (
-  <Root
-    history={history}
-    store={store}
-    debug={__DEBUG__}
-    debugExternal={__DEBUG_NW__}
-  />
+  <Root />
 );
 
 ReactDOM.render(node, target);
